5a226dfba213db651d4a2c87200827bb0ebf6b38
72300663 <72300663@SIGMAB31>
2023-06-14 21:30:33 -0300

    Adicionar arquivos de projeto.

---
+++ questão1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace leitor_de_pag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista_numeros = new List<int>();
+            leitura(lista_numeros);
+            lista_numeros.Sort();
+            escrita(lista_numeros);
+            
+        }
+        static void leitura(List<int> lista_numeros)
+        {
+            try
+            {
+                String line;
+
+                StreamReader sr = new StreamReader("C:\\Users\\72300663\\Documents\\teste2.txt");
+
+                line = sr.ReadLine();
+   
+                while (line != null)
+
+                {
+                    int numero = int.Parse(line);  
+                    lista_numeros.Add(numero);
+
+                    Console.WriteLine(line);
+
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+        static void escrita(List<int> lista_numeros)
+        {
+            try
+            {
+                
+                StreamWriter sw = new StreamWriter("C:\\Users\\72300663\\Documents\\teste.txt");
+
+                foreach(int num in lista_numeros)
+                {
+                    sw.WriteLine(num);
+                }
+
+                
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+    }
+}
---
+++ questão2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace leitor_de_pag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha = 3;
+            while(escolha > 0)
+            {
+                Console.WriteLine(" 0-Sair \n 1-Gravar texto \n 2-Ler texto gravado \n Opção:");
+                escolha = int.Parse(Console.ReadLine());
+                if (escolha == 1)
+                {
+                    Console.WriteLine("Qual a frase ou palavra desejada? \n");
+                    String frase = Console.ReadLine();
+                    escrita(frase);
+                }
+                else if (escolha == 2)
+                {
+                    leitura();
+                }
+                else
+                {
+                    escolha = 0;
+                }
+
+            }
+
+            
+
+        }
+        static void leitura()
+        {
+            try
+            {
+                String line;
+
+                StreamReader sr = new StreamReader("C:\\Users\\72300663\\Documents\\questao2\\questao2.txt");
+
+                line = sr.ReadLine();
+
+                while (line != null)
+
+                {
+
+
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                
+            }
+
+        }
+        static void escrita(String frase)
+        {
+            try
+            {
+                
+                StreamWriter sw = new StreamWriter("C:\\Users\\72300663\\Documents\\questao2\\questao2.txt");
+                sw.WriteLine(frase);
+
+
+
+
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("executado!");
+            }
+
+        }
+    }
+}
